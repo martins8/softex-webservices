d2a7b7f01a1bb376802feed73eb433f6560febe1
Arthur Martins <109447711+martins8@users.noreply.github.com>
2022-12-24 13:27:55 -0300

    Update Calculadora.java

--- webservicejavasoftex/Calculadora.java
+++ webservicejavasoftex/Calculadora.java
@@ -3,11 +3,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package webservicejavasoftex;
-
+import javax.xml.ws.Endpoint;
 /**
  *
  * @author Arthur Martins
  */
 public class Calculadora {
-    
+    public static void main(String[] args)
+  {
+    Endpoint.publish("http://127.0.0.1:9876/calc",
+    new CalculadoraServerImpl());
+  }
 }
